13908ccfc02efb94979341aa3ff983e6a9abf3eb
Frollo <liquidfrollo@gmail.com>
2017-02-24 10:24:28 -0700

    first

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('vlog');
+  this.route('about');
+  this.route('page-not-found',{path: '/*wildcard'});
 });
 
 export default Router;
